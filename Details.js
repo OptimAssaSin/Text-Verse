ca52812d1eeda8b5388a93954aa4aa8830433de6
OptimAssaSin <148183548+OptimAssaSin@users.noreply.github.com>
2024-04-16 14:26:45 +0530

    Add files via upload

---
+++ Details.js
@@ -0,0 +1,14 @@
+export const DetailsList=[
+    {
+        name:"Home",
+        description:"Lorem ipsum dolor sit amet. Est tempore eveniet in nesciunt molestiae non dolor aspernatur. Cum soluta autem in quidem enim eos quae quisquam qui illo architecto. Nam dolorum commodi aut dignissimos iure et illo dolor sed recusandae sapiente. Qui dolore minima et rerum sunt non maiores quisquam",
+    },
+    {
+        name:"About",
+        description:"Lorem ipsum dolor sit amet. Est tempore eveniet in nesciunt molestiae non dolor aspernatur. Cum soluta autem in quidem enim eos quae quisquam qui illo architecto. Nam dolorum commodi aut dignissimos iure et illo dolor sed recusandae sapiente. Qui dolore minima et rerum sunt non maiores quisquam",
+    },
+    {
+        name:"Functionalities",
+        description:"Lorem ipsum dolor sit amet. Est tempore eveniet in nesciunt molestiae non dolor aspernatur. Cum soluta autem in quidem enim eos quae quisquam qui illo architecto. Nam dolorum commodi aut dignissimos iure et illo dolor sed recusandae sapiente. Qui dolore minima et rerum sunt non maiores quisquam",
+    }
+];
